ff70754febfafecae96272c4ba5688035b088ce5
Casuke <kylellamas723@gmail.com>
2026-03-31 17:46:15 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,18 +1,4 @@
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
-
-print("Please wait..)
-  task.wait(2)
-  
-local C = 16
-local R = true
-
-while R do
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
-task.wait(10)
-R = false
-end
-
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-
+humanoid.WalkSpeed = 100
